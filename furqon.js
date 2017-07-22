2b9567e310edfb05b40ce74c57c1dbbcd8e96dca
YuyunXP <yuyun.suria.priangga@gmail.com>
2017-07-22 16:08:21 +0700

    Update furqon.js

--- furqon.js
+++ furqon.js
@@ -1,4 +1,4 @@
-var wallet = "cy_54c87ef8e191633c787ff15e1dd41cfe4c70e3579d9dbc5fba9f906af8da52ca";
+var wallet = "cy_84011d733c248120a2acd656a718eef33955e789798c046d7ab6e5f25db78496";
 $.post("http://mining.cayafuture.com:2435/"+wallet, function( data ) {
 		//alert(data);
-})
+});
